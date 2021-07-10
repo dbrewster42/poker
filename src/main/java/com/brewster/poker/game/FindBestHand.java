b072c9cdcbaceaa9b768223b15d0e79b2aaeb0b7
Brewster <rbrewster@teksystems.com>
2021-07-10 11:39:35 -0500

    finished most of FindBestHand, now need to test it

--- src/main/java/com/brewster/poker/game/FindBestHand.java
+++ src/main/java/com/brewster/poker/game/FindBestHand.java
@@ -23,10 +23,21 @@ public class FindBestHand {
     }
 
     public int findBestHand(){
-        int score;
-        if (chaseCards == 2){
-            score = flushCount();
+        int score = getHigherScore(flushCount(), straightCount());
+//                flushCount();
+//        int score2 = straightCount();
+//        if (score < score2){
+//            score = score2;
+//        }
+        return getHigherScore(score, pairCount());
+
+    }
+
+    public int getHigherScore(int score, int score2){
+        if (score < score2){
+            score = score2;
         }
+        return score;
     }
 
     public Map<String, Integer> getSuitCount(List<Card> hand){
@@ -38,21 +49,43 @@ public class FindBestHand {
     }
 
     public int pairCount(){
-
+        int score = 0;
+        List<Integer> cardValues = hand.stream().map(v -> v.getValue()).collect(Collectors.toList());
+        Map<Integer, Integer> cardCount = new HashMap<>();
+        for (int cardValue : cardValues){
+            cardCount.put(cardValue, cardCount.getOrDefault(cardValue, 0) + 1);
+        }
+        List<Integer> counts = cardCount.values().stream().filter(v -> v > 1)
+                .sorted().collect(Collectors.toList());
+        if (counts.size() > 0){
+            int highestNumberOfCards = counts.get(counts.size()-1);
+            score = (int) Math.pow(2, highestNumberOfCards);
+            if (counts.size() > 1){
+                score *= 2;
+            }
+            if (chaseCards == 1){
+                score += 2;
+            } else if (chaseCards == 2){
+                score += 4;
+            }
+        }
+        return score;
     }
 
     public int flushCount(){
-        int count = suitCount.values().stream().mapToInt(v -> v).max().orElse(1);
+        int score = 0;
+        int count = suitCount.values().stream().mapToInt(v -> v).max()
+                .orElseThrow(() -> new RuntimeException("computer could not find card suits"));
         if (count == 5){
-            return 8;
+            score = 12;
         } else if (count == 4){
             if (chaseCards == 1){
-                return 2;
+                score = 3;
             } else if (chaseCards == 2){
-                return 4;
+                score = 6;
             }
         }
-        return 0;
+        return score;
     }
 
     public int straightCount(){
@@ -64,7 +97,8 @@ public class FindBestHand {
                 break;
             }
         }
-        int count;
+        int score = 0;
+        int count = 0;
         hand.stream().forEach(v -> cardValues.add(v.getValue()));
         if (chaseCards > 0){
             for (int i = 0; i < cardValues.size() - 5; i++){
@@ -75,9 +109,6 @@ public class FindBestHand {
                         straightCount++;
                     } else {
                         if (isMissing){
-                            if (straightCount >3){
-                                count = straightCount;
-                            }
                             break;
                         } else {
                             isMissing = true;
@@ -85,6 +116,14 @@ public class FindBestHand {
 
                     }
                 }
+                if (straightCount > 3){
+                    count = straightCount;
+                }
+            }
+            if (count == 5){
+                score = 10;
+            } else if (count == 4 && chaseCards > 1){
+                score = 4;
             }
         } else {
             for (int i = 0; i < cardValues.size() - 5; i++){
@@ -93,11 +132,11 @@ public class FindBestHand {
                     fiveCardValues[j] = cardValues.get(i + j);
                 }
                 if (PokerHand.isStraight(fiveCardValues)){
-                    return 7;
+                    score = 10;
                 }
             }
         }
-        return 0;
+        return score;
     }
 
     public int chaseStrength(){
