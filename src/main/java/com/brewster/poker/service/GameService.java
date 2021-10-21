98f43448ff40e12098d9ba45caf55da0dc991773
Brewster <rbrewster@teksystems.com>
2021-10-21 13:38:08 -0500

    changed gameservice to abstract class, deciding on how best to proceed

--- src/main/java/com/brewster/poker/service/GameService.java
+++ src/main/java/com/brewster/poker/service/GameService.java
@@ -1,20 +1,208 @@
 package com.brewster.poker.service;
 
+import com.brewster.poker.bet.BetOptions;
+import com.brewster.poker.card.Card;
+import com.brewster.poker.card.PokerHandEnum;
+import com.brewster.poker.card.PokerHandTieBreaker;
+import com.brewster.poker.dto.PlayerDto;
 import com.brewster.poker.dto.UserDto;
+import com.brewster.poker.exception.GameNotFoundException;
+import com.brewster.poker.exception.UserNotFoundException;
 import com.brewster.poker.model.GameEntity;
 import com.brewster.poker.model.request.GameSettingsRequest;
+import com.brewster.poker.model.response.EndRoundResponse;
 import com.brewster.poker.model.response.GameResponse;
 import com.brewster.poker.model.response.NewGameResponse;
 import com.brewster.poker.player.HumanPlayer;
+import com.brewster.poker.player.Player;
+import com.brewster.poker.repository.GameRepository;
+import com.brewster.poker.utility.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public interface GameService {
-    GameEntity createGame(UserDto userDto, GameSettingsRequest settingsRequest);
-    GameEntity findGame(long id);
-    NewGameResponse startNewDeal(GameEntity gameEntity, UserDto userDto);
-    NewGameResponse startNewDeal(GameEntity gameEntity, String email);
-    GameResponse deal(GameEntity gameEntity);
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
-    void addPlayerToGame(GameEntity gameEntity, HumanPlayer player);
-    void saveGame(GameEntity gameEntity);
+public abstract class GameService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GameService.class);
+    final GameRepository gameRepository;
+    final BetService betService;
+    final UserService userService;
+    private long gameId;
+
+    public GameService(GameRepository gameRepository, BetService betService, UserService userService){
+        this.gameRepository = gameRepository;
+        this.betService = betService;
+        this.userService = userService;
+    }
+
+    public abstract GameResponse deal(GameEntity gameEntity);
+    protected abstract void dealPlayerCards(List<Player> players, List<Card> cards);
+
+    public GameEntity createGame(UserDto userDto, GameSettingsRequest settingsRequest){
+        gameId = gameRepository.count() + 1;
+        List<Player> players = generatePlayers(userDto, settingsRequest);
+
+        GameEntity game = new GameEntity(gameId++, players, settingsRequest);
+        return gameRepository.save(game);
+    }
+
+    private List<Player> generatePlayers(UserDto userDto, GameSettingsRequest settingsRequest){
+        List<Player> players;
+        if (settingsRequest.isFillWithComputerPlayers()){
+            UserDto computer = userService.retrieveComputerUser();
+            players = Utils.generateNComputerPlayers(settingsRequest.getNumberOfPlayers() - 1, computer);
+        } else {
+            players = new ArrayList<>();
+        }
+        players.add(new HumanPlayer(settingsRequest.getDisplayName(), userDto));
+        return players;
+    }
+
+    public GameEntity findGame(long id){
+        Optional<GameEntity> gameEntity = gameRepository.findById(id);
+        if (!gameEntity.isPresent()){
+            throw new GameNotFoundException();
+        }
+        return gameEntity.get();
+    }
+
+
+    public void saveGame(GameEntity gameEntity){
+        gameRepository.save(gameEntity);
+    }
+
+    public NewGameResponse startNewDeal(GameEntity gameEntity, UserDto userDto){
+        LOGGER.info("starting new deal with {}", userDto);
+
+        gameEntity.applyNewDeal();
+        dealPlayerCards(gameEntity.getPlayers(), gameEntity.getCards());
+        betService.startNewDeal(gameEntity);
+
+        return getNewGameResponse(gameEntity, userDto);
+    }
+    public NewGameResponse startNewDeal(GameEntity gameEntity, String email){
+        gameEntity.applyNewDeal();
+        dealPlayerCards(gameEntity.getPlayers(), gameEntity.getCards());
+        betService.startNewDeal(gameEntity);
+        UserDto userDto = getThisUser(gameEntity, email);
+
+        return getNewGameResponse(gameEntity, userDto);
+    }
+
+
+    EndRoundResponse calculateWinningHand(GameEntity gameEntity){
+        if (gameEntity.isDealDone() && !gameEntity.isBet()){
+            List<Player> activePlayers = gameEntity.getPlayers();
+            List<Player> winners = new ArrayList<>();
+            List<PlayerDto> playerDtos = new ArrayList<>();
+            int winningStrength = 0;
+            int pot = gameEntity.getBetManagerEntity().getPot();
+            Player winner = null;
+            for (Player player : activePlayers){
+                PokerHandEnum pokerHand = PokerHandEnum.lookupHand(player.getCards());
+                LOGGER.info("{} has a {}", player.getDisplayName(), pokerHand.getHandName());
+                player.setPokerHand(pokerHand);
+                playerDtos.add(new PlayerDto(player));
+                int score = pokerHand.getScore();
+                LOGGER.info("previous winning strength = {} and newcomer's score = {}", winningStrength, score);
+                if (score > winningStrength){
+                    winningStrength = score;
+                    winner = player;
+                    winners = List.of(winner);
+                } else if (winningStrength == score){
+                    LOGGER.info("THERE IS A TIE {}", pokerHand.getHandName());
+                    LOGGER.info("previous winners - {}", winners.size());
+                    if (winners.size() < 2){
+                        winners = PokerHandTieBreaker.getTieBreaker(winner, player);
+                        if (winners.size() == 1){
+                            winner = winners.get(0);
+                        }
+                    } else {
+                        if (PokerHandTieBreaker.getTieBreaker(winner, player).size() > 1){
+                            winners.add(player);
+                        } else {
+                            winner = player;
+                            winners = List.of(winner);
+                        }
+                    }
+                    winners.forEach(v -> LOGGER.info("winner {} ***** !", v.getDisplayName()));
+                }
+            }
+            if (winners.size() > 1){
+                return getTieRoundResponse(winners, playerDtos, pot);
+            }
+            winner.collectWinnings(pot);
+            PlayerDto playerDto = new PlayerDto(winner.getDisplayName(), winner.getPokerHand().getHandName());
+            return new EndRoundResponse(pot, playerDto, playerDtos);
+        }
+        LOGGER.info("{} - {}", gameEntity.isDealDone(), gameEntity.isBet());
+        throw new IllegalArgumentException("Game is still on-going");
+    }
+
+    private EndRoundResponse getTieRoundResponse(List<Player> winners, List<PlayerDto> playerDtos, int pot){
+        winners.forEach(w -> w.collectWinnings(pot / winners.size()));
+
+        StringBuilder stringBuilder = new StringBuilder();
+        winners.forEach(v -> stringBuilder.append(v.getDisplayName() + " and "));
+        stringBuilder.delete(stringBuilder.length() - 5, stringBuilder.length());
+        stringBuilder.append(" have tied and will split the pot of " + pot
+                + "$ with their poker hand of a " + winners.get(0).getPokerHand().getHandName());
+
+        return new EndRoundResponse(stringBuilder.toString(), playerDtos);
+    }
+
+    private NewGameResponse getNewGameResponse(GameEntity gameEntity, UserDto userDto){
+        Player thisPlayer = getThisPlayer(gameEntity, userDto.getEmail());
+        List<Card> playerCards = thisPlayer.getCards();
+        BetOptions options = betService.manageComputerBets(gameEntity);
+        LOGGER.info(options.toString(), playerCards);
+        gameRepository.save(gameEntity);
+        return new NewGameResponse(gameEntity.getId(), playerCards, getUsers(gameEntity, userDto), options, userDto.getMoney());
+    }
+
+    private List<UserDto> getUsers(GameEntity gameEntity, UserDto userDto){
+        List<UserDto> users = new ArrayList<>();
+        for (Player player : gameEntity.getPlayers()){
+            if (!player.getEmail().equals(userDto.getEmail())){
+                users.add(new UserDto(player));
+            }
+        }
+        return users;
+    }
+
+    private UserDto getThisUser(GameEntity gameEntity, String email){
+        Player thisPlayer = gameEntity.getPlayers().stream()
+                .filter(v -> v.getEmail().equals(email))
+                .findAny()
+                .orElseThrow(UserNotFoundException::new);
+
+        return new UserDto(thisPlayer);
+    }
+
+    private Player getThisPlayer(GameEntity gameEntity, String email){
+        return gameEntity.getPlayers().stream()
+                .filter(v -> v.getEmail().equals(email))
+                .findAny()
+                .orElseThrow(UserNotFoundException::new);
+    }
+
+    public void addPlayerToGame(GameEntity gameEntity, HumanPlayer player){
+//          if (desiredNumberOfPlayers == players.size()){
+//               Player playerToRemove = null;
+//               for (Player eachPlayer : players){
+//                    if (eachPlayer.getClass() == ComputerPlayer.class){
+//                         playerToRemove = eachPlayer;
+//                         break;
+//                    }
+//               }
+//               if (playerToRemove != null){
+//                    players.remove(playerToRemove);
+//               }
+//          }
+//          players.add(player);
+//          openSlots--;
+    }
 
 }
--- src/main/java/com/brewster/poker/service/SevenStudService.java
+++ src/main/java/com/brewster/poker/service/SevenStudService.java
@@ -1,55 +1,32 @@
 package com.brewster.poker.service;
 
+import com.brewster.poker.bet.BetOptions;
+import com.brewster.poker.card.Card;
 import com.brewster.poker.dto.UserDto;
 import com.brewster.poker.exception.GameNotFoundException;
+import com.brewster.poker.exception.UserNotFoundException;
 import com.brewster.poker.model.GameEntity;
 import com.brewster.poker.model.request.GameSettingsRequest;
 import com.brewster.poker.model.response.GameResponse;
 import com.brewster.poker.model.response.NewGameResponse;
 import com.brewster.poker.player.HumanPlayer;
+import com.brewster.poker.player.Player;
 import com.brewster.poker.repository.GameRepository;
+import com.brewster.poker.utility.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
-public class SevenStudService implements GameService {
+public class SevenStudService extends GameService {
      private static final Logger LOGGER = LoggerFactory.getLogger(SevenStudService.class);
-     private final GameRepository gameRepository;
-     private final BetService betService;
-     private final UserService userService;
-     private long gameId;
 
      public SevenStudService(GameRepository gameRepository, BetService betService, UserService userService){
-          this.gameRepository = gameRepository;
-          this.betService = betService;
-          this.userService = userService;
-     }
-
-     @Override
-     public GameEntity createGame(UserDto userDto, GameSettingsRequest settingsRequest) {
-          return null;
-     }
-
-     @Override
-     public GameEntity findGame(long id) {
-          Optional<GameEntity> gameEntity = gameRepository.findById(id);
-          if (!gameEntity.isPresent()){
-               throw new GameNotFoundException();
-          }
-          return gameEntity.get();
-     }
-
-     @Override
-     public NewGameResponse startNewDeal(GameEntity gameEntity, UserDto userDto) {
-          return null;
-     }
-
-     @Override
-     public NewGameResponse startNewDeal(GameEntity gameEntity, String email) {
-          return null;
+          super(gameRepository, betService, userService);
      }
 
      @Override
@@ -58,12 +35,18 @@ public class SevenStudService implements GameService {
      }
 
      @Override
-     public void addPlayerToGame(GameEntity gameEntity, HumanPlayer player) {
+     protected void dealPlayerCards(List<Player> players, List<Card> cards) {
 
      }
 
-     @Override
-     public void saveGame(GameEntity gameEntity) {
 
-     }
+//     @Override
+//     public GameEntity createGame(UserDto userDto, GameSettingsRequest settingsRequest) {
+//          gameId = gameRepository.count() + 1;
+//          List<Player> players = generatePlayers(userDto, settingsRequest);
+//
+//          GameEntity game = new GameEntity(gameId++, players, settingsRequest);
+//          return gameRepository.save(game);
+//     }
+
 }
